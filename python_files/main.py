fee2cd84a2bf86566a5c3d03a14b073a914bcf1c
Dmitry Zhartun <dimazhartun454@gmail.com>
2025-10-14 14:43:48 +0300

    resolved issue with parameters main

--- python_files/main.py
+++ python_files/main.py
@@ -1,6 +1,9 @@
 lambda add(a, x): return a+x
 
 if __name__ == "__main__":
-	print(add(4, 5))
+	try:
+		print(add(4, 5))
+	except Exception as e:
+		print("Неверные параметры в add")
 else:
 	print("isn't main")
